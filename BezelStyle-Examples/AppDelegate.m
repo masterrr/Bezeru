50d49b0c31c602e010f05d442b54626804f69e0e
Dmitry Kurilo <me@masterrr.name>
2013-01-04 22:30:14 +0300

    enum with bezel types, loop for generating buttons with no title, description isn't needed

--- BezelStyle-Examples/AppDelegate.m
+++ BezelStyle-Examples/AppDelegate.m
@@ -17,6 +17,29 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    
+    
+    
+    typedef enum bezelTypes {
+        NSRoundedBezelStyle           = 1,
+        NSRegularSquareBezelStyle     = 2,
+        NSThickSquareBezelStyle       = 3,
+        NSThickerSquareBezelStyle     = 4,
+        NSDisclosureBezelStyle        = 5,
+        NSShadowlessSquareBezelStyle  = 6,
+        NSCircularBezelStyle          = 7,
+        NSTexturedSquareBezelStyle    = 8,
+        NSHelpButtonBezelStyle        = 9,
+        NSSmallSquareBezelStyle       = 10,
+        NSTexturedRoundedBezelStyle   = 11,
+        NSRoundRectBezelStyle         = 12,
+        NSRecessedBezelStyle          = 13,
+        NSRoundedDisclosureBezelStyle = 14,
+        NSInlineBezelStyle = 15,
+        NSSmallIconButtonBezelStyle  = 2
+    } NSBezelStyle;
+    
+    NSBezelStyle bezels;
     NSRect frame = [[NSScreen mainScreen] frame];
     NSWindow *fadedWindow = [[NSWindow alloc] initWithContentRect:frame
                                                         styleMask:NSBorderlessWindowMask
@@ -33,20 +56,22 @@
     // We start draw from left bottom corner
     
     // Description to the button
-    NSTextField *description = [[NSTextField alloc] initWithFrame:NSMakeRect(30, 30, 300, 30)];
+    /* NSTextField *description = [[NSTextField alloc] initWithFrame:NSMakeRect(30, 30, 300, 30)];
     [description setFont:[NSFont fontWithName:@"Helvetica" size:19]];
-    [description setStringValue:@"NSSmallSquareBezelStyle"];
     [description setBordered:NO];
     [description setTextColor:[NSColor blackColor]];
     [description setEditable:NO];
-    [description setBackgroundColor:[NSColor clearColor]];
-    
-    NSButton *button = [[NSButton alloc] initWithFrame:NSMakeRect(330, 30, 300, 30)];
-    [button setFont:[NSFont fontWithName:@"Helvetica" size:18]];
-    [button setTitle:@"Push me"];
-    [button setBezelStyle:NSSmallSquareBezelStyle];
-    [[fadedWindow contentView] addSubview:description];
-    [[fadedWindow contentView] addSubview:button];
+    [description setBackgroundColor:[NSColor clearColor]]; */
+    for (int i=1; i<16; i++) {
+        bezels = i;
+        NSButton *button = [[NSButton alloc] initWithFrame:NSMakeRect(330, i*50, 300, 30)];
+        [button setFont:[NSFont fontWithName:@"Helvetica" size:18]];
+        [button setTitle:@""];
+        [button setBezelStyle:bezels];
+        [[fadedWindow contentView] addSubview:button];
+        
+    }
+    //[[fadedWindow contentView] addSubview:description];
     [fadedWindow makeKeyAndOrderFront:self];
     [fadedWindow orderFront:self];
 

c5ed1e03f90e17bed75c744298178c15da8159cf
Dmitry Kurilo <me@masterrr.name>
2013-02-13 04:01:37 +0300

    comments?

--- BezelStyle-Examples/AppDelegate.m
+++ BezelStyle-Examples/AppDelegate.m
@@ -20,7 +20,6 @@
     
     [NSApp enableSuddenTermination];
     [NSApp disableRelaunchOnLogin];
-    
     typedef enum bezelTypes {
         NSRoundedBezelStyle           = 1,
         NSRegularSquareBezelStyle     = 2,
@@ -39,7 +38,6 @@
         NSInlineBezelStyle = 15,
         NSSmallIconButtonBezelStyle  = 2
     } NSBezelStyle;
-    
     NSBezelStyle bezels;
     NSRect frame = [[NSScreen mainScreen] frame];
     NSWindow *fadedWindow = [[NSWindow alloc] initWithContentRect:frame
@@ -48,21 +46,9 @@
                                                         defer:YES
                                                         screen:nil];
     [fadedWindow setAcceptsMouseMovedEvents:YES];
-    // make window transparent
     [fadedWindow setOpaque:NO]; 
     [fadedWindow setLevel:CGShieldingWindowLevel()];
-    // make white overlay, cmd+q to quit
     [fadedWindow setBackgroundColor:[NSColor colorWithDeviceRed:255 green:255 blue:255 alpha:0.9]];
-    
-    // We start draw from left bottom corner
-    
-    // Description to the button
-    /* NSTextField *description = [[NSTextField alloc] initWithFrame:NSMakeRect(30, 30, 300, 30)];
-    [description setFont:[NSFont fontWithName:@"Helvetica" size:19]];
-    [description setBordered:NO];
-    [description setTextColor:[NSColor blackColor]];
-    [description setEditable:NO];
-    [description setBackgroundColor:[NSColor clearColor]]; */
     for (int i=1; i<16; i++) {
         bezels = i;
         NSButton *button = [[NSButton alloc] initWithFrame:NSMakeRect(330, i*50, 300, 30)];
@@ -72,10 +58,8 @@
         [[fadedWindow contentView] addSubview:button];
         
     }
-    //[[fadedWindow contentView] addSubview:description];
     [fadedWindow makeKeyAndOrderFront:self];
     [fadedWindow orderFront:self];
-
 }
 
 @end
